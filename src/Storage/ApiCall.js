490b2d4502d71983e10a2bceb08ca5ba79600200
Rajneesh Shukla <124131361+developerrajneesh@users.noreply.github.com>
2024-01-10 00:23:31 +0530

    Update ApiCall.js

--- src/Storage/ApiCall.js
+++ src/Storage/ApiCall.js
@@ -28,5 +28,36 @@ function ApiCall({children}) {
     <Context.Provider value={{data}}>{children}</Context.Provider>
   )
 }
-export const dataStore =()=> useContext(Context)
+"use client"
+import axios from "axios";
+
+import React,{useEffect,useState,createContext,useContext} from 'react'
+const Context = createContext()
+function ApiCall({children}) {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get(
+            `https://cpapi.rajneesh.site/api/v1/admin/get-blog`
+          );
+  
+          setData(response.data);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      };
+  
+      fetchData();
+    }, []); 
+  
+
+
+  return (
+    <Context.Provider value={{data}}>{children}</Context.Provider>
+  )
+}
+export const DataStore = () => useContext(Context)
+export default ApiCall
 export default ApiCall
